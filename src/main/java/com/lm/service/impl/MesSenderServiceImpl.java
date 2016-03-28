5d57f205d2ced033fc8fc7b71f81afaabe8512fd
zhenghong <zhenghong596@126.com>
2016-03-28 14:15:39 +0800

    事务测试

--- src/main/java/com/lm/service/impl/MesSenderServiceImpl.java
+++ src/main/java/com/lm/service/impl/MesSenderServiceImpl.java
@@ -3,6 +3,7 @@ package com.lm.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lm.model.MesInfo;
 import com.lm.service.MesSenderService;
@@ -15,13 +16,19 @@ public class MesSenderServiceImpl implements MesSenderService{
 	@Autowired
 	private JmsTemplate jmsTemplateTopic;
 
+	@Transactional(readOnly = false, rollbackFor = Exception.class)
 	public void sendQueue(MesInfo mi) {
 
-		jmsTemplateQueue.convertAndSend(mi);
+		jmsTemplateQueue.convertAndSend("100");
+		
+		//int i = 1/0;
+		
+		jmsTemplateQueue.convertAndSend("200");
 	}
 
 	public void sendTopic(MesInfo mi) {
 
 		jmsTemplateTopic.convertAndSend(mi);
 	}
+	
 }
